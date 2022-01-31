bc950dc97e73fe461e7e1f40870e237b91b1546f
cnrakt <82604970+cnrakt@users.noreply.github.com>
2022-01-31 23:31:01 +0300

    Update classparsimnet.r
    Bug fixed in 'pieplot' methods (Thanks to Dr. Jakob Mueller from Max Planck Institute for Ornithology for reporting this bug)

--- R/classparsimnet.r
+++ R/classparsimnet.r
@@ -925,7 +925,7 @@ setMethod(f="pieplot", signature= c("Parsimnet","Haplotype"), definition=functio
     for(i in 1: nhap)
     {
         
-        args.flp <- c(list(xpos = cx[i],ypos=cy[i],x=g[i,], radius=rds[i],col= cls,startpos=0),c(dots2,list(main=nm)))
+        args.flp <- c(list(xpos = cx[i],ypos=cy[i],x=g[i,][g[i,]>0], radius=rds[i],col= cls,startpos=0),c(dots2,list(main=nm)))
         do.call(localfloating.pie,  args.flp)
     }
     
